c0e3b28f286befa9753a9abe1847f7934c94be51
Muhammad-2021 <81512114+Muhammad-2021@users.noreply.github.com>
2021-10-02 20:13:29 +0200

    push java branch

--- app/src/main/java/com/falcon/restaurants/utils/Constants.java
+++ app/src/main/java/com/falcon/restaurants/utils/Constants.java
@@ -2,7 +2,7 @@ package com.falcon.restaurants.utils;
 
 public class Constants {
 
-    public static final String DOMAIN_URL = "http://10.0.2.2/" ;
+    //public static final String DOMAIN_URL = "http://10.0.2.2/" ;
     //public static final String domainUrl = "http://192.168.1.6/" ;
     //public static final String BASE_URL = DOMAIN_URL + "restaurantsserver/" ;
     public static final String BASE_URL = "https://restaurantsserver.000webhostapp.com/" ;
